7b6b96396d74abfc93fcb89ee6f8527ad843d60b
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-12-15 17:08:00 -0800

    can navigate to booklist and back to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import BookList from './views/Books/BookList';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/books">
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
